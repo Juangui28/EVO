f336909134ccf691734d10f3b80a87279fd4fe98
Juangui28 <jugulome28@gmail.com>
2023-11-10 16:46:53 -0500

    - Se ha creado el proyecto en un nuevo repositorio, el anterior no nos funcionó bien
    - Se ha agregado la animación principal
    
    - El icono de la app se ha modificado

---
+++ app/src/main/java/com/juanguillermo/evo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.juanguillermo.evo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
