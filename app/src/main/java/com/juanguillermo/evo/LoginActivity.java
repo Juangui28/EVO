58a625440db410574f0c7054e039e6f7154b018e
Juangui28 <jugulome28@gmail.com>
2023-11-17 17:57:39 -0500

    Se ha intentado crear la bd

--- app/src/main/java/com/juanguillermo/evo/LoginActivity.java
+++ app/src/main/java/com/juanguillermo/evo/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.juanguillermo.evo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,6 +8,7 @@ import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Patterns;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -14,12 +16,25 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
+
 public class LoginActivity extends AppCompatActivity {
 
-    EditText username;
-    EditText password;
+    EditText loginUsername, loginPassword;
     Button loginButton;
-    TextView signUpText, exploreText;
+    TextView signupRedirectText, exploreRedirectText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,36 +50,21 @@ public class LoginActivity extends AppCompatActivity {
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         }
 
-        username = findViewById(R.id.username);
-        password = findViewById(R.id.password);
+        loginUsername = findViewById(R.id.usernameLogin);
+        loginPassword = findViewById(R.id.passwordLogin);
         loginButton = findViewById(R.id.loginButton);
-        signUpText = findViewById(R.id.signupText);
-        exploreText = findViewById(R.id.exploreText);
+        exploreRedirectText = findViewById(R.id.exploreText);
+        signupRedirectText = findViewById(R.id.signupText);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(username.getText().toString().equals("user") && password.getText().toString().equals("1234")){
-                    new Handler().postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            startActivity(new Intent(LoginActivity.this,MainActivity.class));
-                            finish();
-                        }
-                    },1000);
-                }else {
-                    Toast.makeText(LoginActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-        signUpText.setOnClickListener(new View.OnClickListener() {
+
+        signupRedirectText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(LoginActivity.this, RegistroActivity.class));
             }
         });
 
-        exploreText.setOnClickListener(new View.OnClickListener() {
+        exploreRedirectText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(LoginActivity.this, MainActivity.class));
--- app/src/main/java/com/juanguillermo/evo/RegistroActivity.java
+++ app/src/main/java/com/juanguillermo/evo/RegistroActivity.java
@@ -1,5 +1,6 @@
 package com.juanguillermo.evo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,43 +9,60 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegistroActivity extends AppCompatActivity {
 
-    TextView signInText, exploreText;
+    EditText signupUsername, signupEmail,signupPassword;
+    TextView loginRedirectText, exploreRedirectText;
+    Button signupButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
 
-        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP){
+        //Esta parte del codigo es para que la app ocupe toda la pantalla
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
             getWindow().setStatusBarColor(Color.TRANSPARENT);
-        }else {
+        } else {
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         }
 
-        signInText = findViewById(R.id.signInText);
-        exploreText = findViewById(R.id.exploreText);
+        signupUsername = findViewById(R.id.usernameSignup);
+        signupEmail = findViewById(R.id.emailSignup);
+        signupPassword = findViewById(R.id.passwordSignup);
+        signupButton = findViewById(R.id.registerButton);
+        loginRedirectText = findViewById(R.id.signInText);
+        exploreRedirectText = findViewById(R.id.exploreText);
 
-        signInText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(RegistroActivity.this, LoginActivity.class));
-            }
-        });
-
-        exploreText.setOnClickListener(new View.OnClickListener() {
+        exploreRedirectText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(RegistroActivity.this, MainActivity.class));
             }
         });
 
+        loginRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegistroActivity.this, LoginActivity.class));
+            }
+        });
 
     }
 }
